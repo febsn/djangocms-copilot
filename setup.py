8d98a3a2fd23c84de120dcec392b998e279ef6eb
Fabian Lehner <fl@makonis.net>
2016-02-13 10:03:48 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,34 @@
+from pip.req import parse_requirements
+from distutils import stup, find_packages
+
+setup(
+    name="django-copilot",
+    version="0.0.1.dev1",
+    description="Django-CMS apphook and API client for the Copilot management software public API",
+    author="Fabian Lehner",
+    author_email="fl@makonis.net",
+    license="MIT",
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        'Framework :: Django :: 1.8',
+
+        'Intended Audience :: Developers',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+
+         'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    packages=find_packages(),
+    install_requires=['requests', 'django-appconf'],
+)
