a4d7d7a1af61e8a21de66ac3ae2d8787b4039d60
Fabian Lehner <fl@makonis.net>
2016-03-31 19:05:27 +0200

    Make artist assets filterable by keyword (media.keyword)

--- copilot/artists/api_models.py
+++ copilot/artists/api_models.py
@@ -13,6 +13,7 @@ class Artist(object):
         self.client = CopilotClient(settings.COPILOT_USER)
         self.id = id
         self._get()
+        self._assign_assets()
 
     def _get(self):
         data = self.client.get('artists/{id}'.format(id=self.id)).json()
@@ -24,6 +25,16 @@ class Artist(object):
             except KeyError:
                 logger.warning("Property {prop} is missing in copilot API "
                     "response for Artist {id}".format(prop=prop, id=self.id))
+
+    def _assign_assets(self):
+        self.media = {}
+        for tag in settings.COPILOT_ASSET_TAGS:
+            self.media[tag] = []
+            for asset in self.assets:
+                for asset_tag in asset['tags']:
+                    if asset_tag['tagName'] == tag:
+                        self.media[tag].append(asset)
+                        break
 
     def news(self, page=1):
         return self.client.get_paginated('artists/{id}/newsEntries'.format(
@@ -45,4 +56,11 @@ class Artist(object):
                 endpoint += '/'+end_date.isoformat()
         return self.client.get_paginated('artists/{id}/events'.format(
             id=self.id), page=page).json()
+
+    def images(self):
+        for asset in self.assets:
+            if asset['mimeType'].startswith('image'):
+                yield asset
+            continue
+
 
--- copilot/conf.py
+++ copilot/conf.py
@@ -6,6 +6,7 @@ from appconf import AppConf
 
 class CopilotConf(AppConf):
     PAGE_SIZE = 30
+    ASSET_TAGS = ['ARTIST_MAINIMAGE', 'ARTIST_PRESSIMAGE', ]
 
     class Meta:
         prefix = 'copilot'
