9217f2d75622764b2fbf4d11e45f7a102bb7b4d6
Fabian Lehner <fl@makonis.net>
2016-04-05 13:36:17 +0200

    EventManager converts to python dates, Apphook uses EventManager

--- copilot/events/api_models.py
+++ copilot/events/api_models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from collections import defaultdict
-from datetime import date
+from datetime import datetime, date
 
 from copilot.conf import settings
 from copilot.api import CopilotClient
@@ -11,26 +11,30 @@ logger = logging.getLogger('djangocms-copilot')
 
 
 class EventManager(object):
+    ALL_ENDPOINT = 'events/'
+    ARTIST_ENDPOINT = 'artists/{id}/events/'
+    SORTING = 'dateOfEvent,asc'
+    FROM = 'from/{}/'
+    ARTIST_FROM = '{}/'
+    TO = 'to/{}/'
+    ARTIST_TO = '{}/'
 
     def __init__(self, artist_id=None, **kwargs):
         super(EventManager, self).__init__(**kwargs)
         self.client = CopilotClient(settings.COPILOT_USER)
         self.artist_id = artist_id
-        self._init_years()
 
-    def _init_years(self):
-        class YearManager(dict):
-            def __init__(self, mgr):
-                self.mgr = mgr
-            def __getitem__(self, key):
-                logger.debug('Events called for year {}'.format(key))
-                endpoint = self.mgr._get_endpoint(
-                    start_date = date(key, 1, 1),
-                    end_date = date(key, 12, 31)
-                )
-                return self.mgr._get(endpoint)
+    def _get_from(self):
+        if self.artist_id:
+            return self.ARTIST_FROM
+        else:
+            return self.FROM
 
-        self.years = YearManager(self)
+    def _get_to(self):
+        if self.artist_id:
+            return self.ARTIST_TO
+        else:
+            return self.TO
 
     def _get_endpoint(self, **kwargs):
         if self.artist_id:
@@ -38,24 +42,66 @@ class EventManager(object):
         else:
             endpoint = self._get_endpoint_for_all(**kwargs)
 
-        if kwargs['start_date']:
-            endpoint += '/'+kwargs['start_date'].isoformat()
-            if kwargs['end_date']:
-                endpoint += '/'+kwargs['end_date'].isoformat()
+        try:
+            endpoint += self._get_from().format(kwargs['start_date'].isoformat())
+            endpoint += self._get_to().format(kwargs['end_date'].isoformat())
+        except KeyError:
+            # no problem if start_date or end_date not given
+            pass
         return endpoint
 
     def _get_endpoint_for_all(self, **kwargs):
-        return 'events'
+        return self.ALL_ENDPOINT
 
     def _get_endpoint_for_artist(self, **kwargs):
-        return 'artists/{id}/events'.format(id=self.artist_id)
+        return self.ARTIST_ENDPOINT.format(id=self.artist_id)
 
     def _get_sorting(self):
-        return 'dateOfEvent,asc'
+        return self.SORTING
 
     def _get(self, endpoint):
         logger.debug('API call: {}'.format(endpoint))
         kwargs = {
             'page.sort': self._get_sorting(),
+            'external': False,
         }
-        return self.client.get_paginated(endpoint, **kwargs).json()
+        events = self.client.get_paginated(endpoint, **kwargs).json()
+        for event in events['content']:
+            try:
+                event['dateOfEvent'] = datetime.strptime(event['dateOfEvent']+event['start'], '%Y-%m-%d%H:%M:%S.%f')
+            except KeyError:
+                try:
+                    event['dateOfEvent'] = datetime.strptime(event['dateOfEvent'], '%Y-%m-%d')
+                except KeyError:
+                    pass
+        return events
+
+    def _get_years(self):
+        endpoint = self._get_endpoint()
+        events = self._get(endpoint)
+        years = defaultdict(list)
+        for event in events:
+            years[event['dateOfEvent'].year].append(event)
+        return years
+
+    @property
+    def years(self):
+        try:
+            return self._years
+        except AttributeError:
+            self._years = self._get_years()
+            return self._years
+
+    def all(self):
+        return self._get(self._get_endpoint())
+
+    def upcoming(self):
+        endpoint = self._get_endpoint(
+            start_date=datetime.now().date())
+        return self._get(endpoint)
+
+    def __str__(self):
+        if self.artist_id:
+            return 'EventManager for {}'.format(self.artist_id)
+        else:
+            return 'EventManager'
--- copilot/events/views.py
+++ copilot/events/views.py
@@ -1,19 +1,18 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from ..base_views import CopilotListView
 from django.utils.timezone import now
+
+from ..base_views import CopilotListView
+from .api_models import EventManager
 
 class EventListView(CopilotListView):
 
     template_name = 'copilot/events/list.html'
 
-    def _upcoming(self, **kwargs):
-        return self._get_paginated('events/from/{}/'.format(
-            now().date().isoformat(), **kwargs)).json()
-
     def get_context_data(self, **kwargs):
         context = super(EventListView, self).get_context_data(**kwargs)
-        data = self._upcoming(**kwargs)
+        manager = EventManager()
+        data = manager.all()
         context['events'] = data['content']
         return context
